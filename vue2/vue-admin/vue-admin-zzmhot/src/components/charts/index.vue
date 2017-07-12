b625dd35598be9231cc4b1a73608ecbd0ad684e9
stevewzx <421320649@qq.com>
2017-07-12 12:24:15 +0800

    vue admin

---
+++ vue2/vue-admin/vue-admin-zzmhot/src/components/charts/index.vue
@@ -0,0 +1,78 @@
+<!--
+/**
+ * @file: index.
+ * @intro: intro.
+ * @author: zzmhot.
+ * @email: zzmhot@163.com.
+ * @Date: 2017/7/3 15:55.
+ *
+ */
+-->
+<template>
+  <div
+    v-loading="load_data"
+    element-loading-text="拼命加载中"></div>
+</template>
+<script type="text/javascript">
+  export default{
+    props: {
+      option: {
+        type: Object,
+        default: null
+      }
+    },
+    data(){
+      return {
+        charts: null,
+        load_data: true
+      }
+    },
+    created(){
+      this.initCharts()
+    },
+    methods: {
+      //初始化charts
+      initCharts(){
+        /**
+         * 按需引入 ECharts 图表和组件，这里先全部引入
+         * doc: http://echarts.baidu.com
+         */
+        this.load_data = true
+        import('echarts')
+          .then(({init}) => {
+            this.$nextTick(() => {
+              const {$el, setOption, option} = this
+              this.charts = init($el)
+              this.load_data = false
+              this.charts.showLoading()
+              option && setOption()
+            })
+          })
+          .catch(() => {
+          })
+      },
+      //设置属性到charts
+      setOption(){
+        const {charts, option} = this
+        if (charts && option) {
+          charts.setOption(option)
+          charts.hideLoading()
+          this.$emit('init', {charts, option})
+        }
+      }
+    },
+    destroyed(){
+      this.charts = null
+    },
+    watch: {
+      option: {
+        handler(){
+          this.setOption()
+        },
+        deep: true
+      }
+    }
+  }
+</script>
+<style lang="scss" type="text/scss" rel="stylesheet/scss">
+</style>
---
+++ vue2/vue-admin/vue-admin/src/mock/data/user.js
@@ -0,0 +1,25 @@
+import Mock from 'mockjs';
+const LoginUsers = [
+  {
+    id: 1,
+    username: 'admin',
+    password: '123456',
+    avatar: 'https://avatars0.githubusercontent.com/u/5573837?v=3&s=460',
+    name: 'SteveWzx'
+  }
+];
+
+const Users = [];
+
+for (let i = 0; i < 86; i++) {
+  Users.push(Mock.mock({
+    id: Mock.Random.guid(),
+    name: Mock.Random.cname(),
+    addr: Mock.mock('@county(true)'),
+    'age|18-60': 1,
+    birth: Mock.Random.date(),
+    sex: Mock.Random.integer(0, 1)
+  }));
+}
+
+export { LoginUsers, Users };
