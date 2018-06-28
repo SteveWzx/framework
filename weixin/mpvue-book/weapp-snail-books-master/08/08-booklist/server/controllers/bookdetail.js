e67a5cdd800cb8d2624630c42a3839b92c4a97d6
steve <421320649@qq.com>
2018-06-28 09:43:32 +0800

    weixin

--- weixin/mpvue-book/weapp-snail-books-master/08/08-booklist/server/controllers/bookdetail.js
+++
@@ -1,8 +0,0 @@
-const {mysql} = require('../qcloud')
-
-module.exports = async (ctx)=>{
-  const {id} = ctx.request.query
-  await mysql('books')
-          .where('id',id)
-          .increment('count',1)
-}
